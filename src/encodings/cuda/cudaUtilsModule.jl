1f82fbe28d3a443b3169805cd82431563fedeb82
una-dinosauria <jltmtzc@gmail.com>
2017-06-27 12:42:39 -0700

    porting cuda wrapper to new CUDArt

--- src/encodings/cuda/cudaUtilsModule.jl
+++ src/encodings/cuda/cudaUtilsModule.jl
@@ -1,9 +1,11 @@
 module CudaUtilsModule
 
+import CUDAdrv: CuModule, CuModuleFile, unsafe_unload!,
+                CuFunction, cudacall
 using CUDArt
 
 const ptxdict = Dict()
-const mdlist = Array(CuModule, 0)
+const mdlist = Array{CuModule}(0)
 
 function mdinit(devlist)
   global ptxdict
@@ -11,25 +13,25 @@ function mdinit(devlist)
   isempty(mdlist) || error("mdlist is not empty")
   for dev in devlist
     device(dev)
-    md = CuModule("src/encodings/cuda/cudautils.ptx", false) # false means it will not be automatically finalized
+    md = CuModuleFile("src/encodings/cuda/cudautils.ptx")
 
     # Utils functions
     ptxdict[(dev, "setup_kernel")] = CuFunction(md, "setup_kernel")
     ptxdict[(dev, "perturb")]      = CuFunction(md, "perturb")
-    ptxdict[(dev, "veccost")]      = CuFunction(md, "veccost")
+    # ptxdict[(dev, "veccost")]      = CuFunction(md, "veccost")
     ptxdict[(dev, "veccost2")]     = CuFunction(md, "veccost2")
     ptxdict[(dev, "vec_add")]      = CuFunction(md, "vec_add")
 
     # ICM functions
-    ptxdict[(dev, "condition_icm")]  = CuFunction(md, "condition_icm");
-    ptxdict[(dev, "condition_icm2")] = CuFunction(md, "condition_icm2");
+    # ptxdict[(dev, "condition_icm")]  = CuFunction(md, "condition_icm");
+    # ptxdict[(dev, "condition_icm2")] = CuFunction(md, "condition_icm2");
     ptxdict[(dev, "condition_icm3")] = CuFunction(md, "condition_icm3");
 
     push!(mdlist, md)
   end
 end
 
-mdclose() = (for md in mdlist; unload(md); end; empty!(mdlist); empty!(ptxdict))
+mdclose() = (for md in mdlist; unsafe_unload!(md); end; empty!(mdlist); empty!(ptxdict))
 
 function finit( )
   mdclose()
@@ -39,38 +41,38 @@ function init( devlist )
   mdinit( devlist )
 end
 
-# Accumulates binaries to unaries for icm conditioning
-function condition_icm(
-  nblocks::Integer, nthreads::Integer,
-  d_ub::CudaArray{Cfloat, 2},     # in/out. h-by-n. where we accumulate
-  d_bb::CudaArray{Cfloat, 2},     # in. binaries
-  d_codek::CudaArray{Cuchar, 1},  # in. n-long. indices into d_bb
-  n::Cint,                        # in. size( d_ub, 2 )
-  h::Cint)                        # in. size( d_ub, 1 )
+# # Accumulates binaries to unaries for icm conditioning
+# function condition_icm(
+#   nblocks::Integer, nthreads::Integer,
+#   d_ub::CudaArray{Cfloat, 2},     # in/out. h-by-n. where we accumulate
+#   d_bb::CudaArray{Cfloat, 2},     # in. binaries
+#   d_codek::CudaArray{Cuchar, 1},  # in. n-long. indices into d_bb
+#   n::Cint,                        # in. size( d_ub, 2 )
+#   h::Cint)                        # in. size( d_ub, 1 )
+#
+#   fun = ptxdict[(device(), "condition_icm")];
+#
+#   cudacall( fun, nblocks, nthreads,
+#     (d_ub, d_bb, d_codek, n, h));
+#
+#   return nothing
+# end
 
-  fun = ptxdict[(device(), "condition_icm")];
-
-  CUDArt.launch( fun, nblocks, nthreads,
-    (d_ub, d_bb, d_codek, n, h));
-
-  return nothing
-end
-
-function condition_icm2(
-  nblocks::Integer, nthreads::CUDArt.CudaDims,
-  d_ub::CudaArray{Cfloat, 2},     # in/out. h-by-n. where we accumulate
-  d_bb::CudaArray{Cfloat, 2},     # in. binaries
-  d_codek::CudaArray{Cuchar, 1},  # in. n-long. indices into d_bb
-  n::Cint,                        # in. size( d_ub, 2 )
-  h::Cint)                        # in. size( d_ub, 1 )
-
-  fun = ptxdict[(device(), "condition_icm2")];
-
-  CUDArt.launch( fun, nblocks, nthreads,
-    (d_ub, d_bb, d_codek, n, h));
-
-  return nothing
-end
+# function condition_icm2(
+#   nblocks::Integer, nthreads::CUDArt.CudaDims,
+#   d_ub::CudaArray{Cfloat, 2},     # in/out. h-by-n. where we accumulate
+#   d_bb::CudaArray{Cfloat, 2},     # in. binaries
+#   d_codek::CudaArray{Cuchar, 1},  # in. n-long. indices into d_bb
+#   n::Cint,                        # in. size( d_ub, 2 )
+#   h::Cint)                        # in. size( d_ub, 1 )
+#
+#   fun = ptxdict[(device(), "condition_icm2")];
+#
+#   cudacall( fun, nblocks, nthreads,
+#     (d_ub, d_bb, d_codek, n, h));
+#
+#   return nothing
+# end
 
 function condition_icm3(
   nblocks::Integer, nthreads::CUDArt.CudaDims,
@@ -83,13 +85,9 @@ function condition_icm3(
 
   fun = ptxdict[(device(), "condition_icm3")];
 
-  CUDArt.launch( fun, nblocks, nthreads,
-    (d_ub,
-    d_bbs,
-    d_codek,
-    conditioning,
-    m,
-    n));
+  cudacall( fun, nblocks, nthreads,
+    (Ptr{Cfloat}, Ptr{Cfloat}, Ptr{Cuchar}, Ptr{Cint}, Cint, Cint),
+    (d_ub, d_bbs, d_codek, conditioning, m, n));
 
   return nothing
 end
@@ -102,23 +100,24 @@ function vec_add(
   h::Cint)
 
   fun = ptxdict[( device(), "vec_add" )];
-  CUDArt.launch( fun, nblocks, nthreads,
+  cudacall( fun, nblocks, nthreads,
+    ( Ptr{Cfloat}, Ptr{Cfloat}, Cint, Cint ),
     ( d_matrix, d_vec, n, h ));
 end
 
-function veccost(
-  nblocks::CUDArt.CudaDims, nthreads::CUDArt.CudaDims,
-  d_rx::CudaArray{Cfloat},
-  d_codebooks::CudaArray{Cfloat},
-  d_codes::CudaArray{Cuchar},
-  d_veccost::CudaArray{Cfloat}, # out.
-  m::Cint,
-  n::Cint)
-
-  fun = ptxdict[( device(), "veccost" )];
-  CUDArt.launch( fun, nblocks, nthreads,
-    ( d_rx, d_codebooks, d_codes, d_veccost, m, n ));
-end
+# function veccost(
+#   nblocks::CUDArt.CudaDims, nthreads::CUDArt.CudaDims,
+#   d_rx::CudaArray{Cfloat},
+#   d_codebooks::CudaArray{Cfloat},
+#   d_codes::CudaArray{Cuchar},
+#   d_veccost::CudaArray{Cfloat}, # out.
+#   m::Cint,
+#   n::Cint)
+#
+#   fun = ptxdict[( device(), "veccost" )];
+#   cudacall( fun, nblocks, nthreads,
+#     ( d_rx, d_codebooks, d_codes, d_veccost, m, n ));
+# end
 
 function veccost2(
   nblocks::CUDArt.CudaDims, nthreads::CUDArt.CudaDims,
@@ -131,9 +130,10 @@ function veccost2(
   n::Cint)
 
   fun = ptxdict[( device(), "veccost2" )];
-  CUDArt.launch( fun, nblocks, nthreads,
+  cudacall( fun, nblocks, nthreads,
+    ( Prt{Cfloat}, Ptr{Cfloat}, Ptr{Cuchar}, Ptr{Cfloat}, Cint, Cint, Cint),
     ( d_rx, d_codebooks, d_codes, d_veccost, d, m, n ),
-    shmem_bytes=Int( d*sizeof(Cfloat)) );
+    shmem=Int( d*sizeof(Cfloat)) );
 end
 
 function perturb(
@@ -145,7 +145,8 @@ function perturb(
   k::Cint)
 
   fun = ptxdict[( device(), "perturb" )];
-  CUDArt.launch( fun, nblocks, nthreads,
+  cudacall( fun, nblocks, nthreads,
+    ( CudaPtr, Ptr{Cuchar}, Cint, Cint, Cint ),
     ( state, codes, n, m, k ));
 end
 
@@ -155,7 +156,8 @@ function setup_kernel(
   state::CudaPtr )
 
   fun = ptxdict[( device(), "setup_kernel" )];
-  CUDArt.launch( fun, nblocks, nthreads,
+  cudacall( fun, (nblocks,), (nthreads,),
+    ( Cint, CudaPtr ),
     ( n, state ));
 end
 
