10e20b7ebeef80f95132cf07f0d479cf0c30fc5c
una-dinosauria <jltmtzc@gmail.com>
2017-11-09 16:59:40 -0800

    add -O3 flag to search. Makes it ~4x faster

--- src/linscan/cpp/compile.sh
+++ src/linscan/cpp/compile.sh
@@ -2,7 +2,7 @@
 # Compile C methods for linear scan
 
 # PQ / OPQ-based
-g++ -shared -fPIC linscan_aqd.cpp -o linscan_aqd.so -fopenmp
+g++ -shared -O3 -fPIC linscan_aqd.cpp -o linscan_aqd.so -fopenmp
 
 # Allocating one extra byte for the dbnorm
-g++ -shared -fPIC linscan_aqd_pairwise_byte.cpp -o linscan_aqd_pairwise_byte.so -fopenmp
+g++ -shared -O3 -fPIC linscan_aqd_pairwise_byte.cpp -o linscan_aqd_pairwise_byte.so -fopenmp
